aa81d4709ff3ef9d284b0731d5080f32d7e99b04
Prateek Bansal <devopslearnnz@gmail.com>
2023-05-17 17:56:31 +1200

    create resource group using terraform config file

--- terraform-manifests/azure-resource-group.tf
+++ terraform-manifests/azure-resource-group.tf
@@ -1,11 +1,20 @@
 # Terraform Settings Block
+# terraform {
+#   required_version = ">= 1.0.0"
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = ">= 2.0" # Optional but recommended in production
+#     }    
+#   }
+# }
+
 terraform {
-  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.0" # Optional but recommended in production
-    }    
+      source  = "hashicorp/azurerm"
+      version = "3.56.0"
+    }
   }
 }
 
@@ -16,6 +25,6 @@ provider "azurerm" {
 
 # Create Resource Group 
 resource "azurerm_resource_group" "my_demo_rg1" {
-  location = "eastus"
-  name = "my-demo-rg1"  
+  location = "australiaeast"
+  name     = "my-demo-rg2"
 }
